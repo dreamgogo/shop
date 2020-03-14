e6056d681d56aae4d3faf933404987f407221e36
ivan <1501816519@qq.com>
2020-03-14 17:02:06 +0800

    init shop

---
+++ src/views/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>购物车</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>我的</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
